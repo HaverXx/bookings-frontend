5ac47db621f3cfbf2fe62deae95bd9b4142231f8
juanpabloniguez <juanpabloniguez@gmail.com>
2026-05-12 10:35:51 +0200

    Delete (admin) directory

--- (admin)/bookings/page.tsx
+++
@@ -1,8 +0,0 @@
-import BookingsClient from "./BookingsClient";
-import { getAppointments } from "@/lib/api";
-
-export default async function BookingsPage() {
-  const bookings = await getAppointments();
-
-  return <BookingsClient initialBookings={bookings} />;
-}
